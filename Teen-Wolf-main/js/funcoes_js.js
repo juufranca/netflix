e1e3913cfa7318f48ef51de91fe80fe71203bee9
juufranca <juhgfranca2006@hotmail.com>
2006-06-27 09:45:06 -0300

    juliana

---
+++ Teen-Wolf-main/js/funcoes_js.js
@@ -0,0 +1,28 @@
+var nome = 'Juliana'             //String
+var idade = 27                   //int
+var semdefinicao                 //undefined
+var nula = null                  //objeto (?!)
+ 
+const nota = 0                   //int
+const sobrenome = 'frança'       //string
+const naodefinida                //undefined
+
+let x = 3
+let jogo = 'Mario'
+let icia
+
+//Operadores Lógicos
+// = (Atribuição)
+// == (Igualdade)
+// === (Igualdade escrita)
+//&& (and)
+//|| (or)
+//!  (not)
+
+x = y                        //atribui y a x
+var var1 = 3                 //atribui 3 a variavel var1
+3 == var1                    //true
+"3" == var1                  //true
+3 == '3'                     //true
+3 === var1                   //true
+3 === '3'                    //false
